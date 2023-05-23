4e3d8c2a636be4e658c76073a143a87d2e6ae6b8
Narendra Singh <narendrasinghks2019@gmail.com>
2023-05-23 11:31:41 +0530

    Added onSubmit functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 // packages
 import ReCAPTCHA from 'react-google-recaptcha';
+import { useRef } from 'react';
 // css
 import './App.css';
 
 const App = () => {
+  const recaptchaRef = useRef<ReCAPTCHA | null>(null);
+
+  const onChange = (value: string | null) => {
+    console.log('Captcha value:', value);
+  };
+
+  const onSubmit = async () => {
+    const token = await recaptchaRef.current?.getValue();
+
+    if (token) {
+      console.log('Captcha token:', token);
+    } else {
+      console.log('Please complete the reCAPTCHA.');
+    }
+  }
+
   return (
     <div>
       <ReCAPTCHA
+        ref={recaptchaRef}
         sitekey='6LdLrDAmAAAAAOYtjQs6lTLMpCx9Kb1aYPmHdeOo'
+        onChange={onChange}
       />
+
+      <button onClick={onSubmit}>Submit</button>
     </div>
   );
 };
