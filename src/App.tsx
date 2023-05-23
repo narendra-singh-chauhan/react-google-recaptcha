a314110d822f8cd12a551a2400d13e49f5aa052e
Narendra Singh <narendrasinghks2019@gmail.com>
2023-05-23 10:46:34 +0530

    Added react-google-recaptcha project on git.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// packages
+import ReCAPTCHA from 'react-google-recaptcha';
+// css
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <ReCAPTCHA
+        sitekey='6LfWaC0mAAAAABLa4zE4WU8APrLQLgOOevQA1qLn'
+      />
+    </div>
+  );
+};
+
+export default App;
